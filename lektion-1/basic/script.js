37e23e4b46a7186485acf8a7b10cafb3d7292072
David Lindström <david.s.lindstrom@gmail.com>
2021-10-06 15:19:35 +0200

    main

--- lektion-1/basic/script.js
+++ lektion-1/basic/script.js
@@ -3,3 +3,10 @@ console.warn ("Detta är en varning")
 console.error ("Detta är error")
 
 document.getElementById("fullName").innerText = "David Lindström"
+
+let firstName = "David"
+let lastName = 'Lindstrom'
+
+var sentence = "Hej jag heter " + firstName + " " + lastName + "."
+console.log(sentence);
+
