08ad8bfdb7d5574bd5f32a29e5a008f861d759e7
David Lindström <david.s.lindstrom@gmail.com>
2021-10-20 22:11:32 +0200

    validationJS

---
+++ validationJS/js/script.js
@@ -0,0 +1,39 @@
+document.getElementById("firstName",).addEventListener("focus", function(e) {
+    document.getElementById("labelFirstName").style.fontSize = "0.9rem"
+})
+
+document.getElementById("firstName",).addEventListener("blur", function(e) {
+    document.getElementById("labelFirstName").style.fontSize = ""
+})
+
+document.getElementById("lastName",).addEventListener("focus", function(e) {
+    document.getElementById("labelLastName").style.fontSize = "0.9rem"
+})
+
+document.getElementById("lastName",).addEventListener("blur", function(e) {
+    document.getElementById("labelLastName").style.fontSize = ""
+})
+
+document.getElementById("password",).addEventListener("focus", function(e) {
+    document.getElementById("labelPassword").style.fontSize = "0.9rem"
+})
+
+document.getElementById("password",).addEventListener("blur", function(e) {
+    document.getElementById("labelPassword").style.fontSize = ""
+})
+
+document.getElementById("confirmPassword",).addEventListener("focus", function(e) {
+    document.getElementById("labelConfirmPassword").style.fontSize = "0.9rem"
+})
+
+document.getElementById("confirmPassword",).addEventListener("blur", function(e) {
+    document.getElementById("labelConfirmPassword").style.fontSize = ""
+})
+
+document.getElementById("email",).addEventListener("focus", function(e) {
+    document.getElementById("labelEmail").style.fontSize = "0.9rem"
+})
+
+document.getElementById("email",).addEventListener("blur", function(e) {
+    document.getElementById("labelEmail").style.fontSize = ""
+})
